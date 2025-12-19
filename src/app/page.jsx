ef64fde1e1bf6d15485e615fa4db1f0165246e43
riday-kumar <hridoykumarsaha07@gmail.com>
2025-12-20 01:54:14 +0600

    next js project setup done and new route created

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-4xl font-bold">Welcome to DevStory</p>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="p-4 border-b-2 border-gray-500">
+      <h2 className="text-lg font-semibold">Dev-Story</h2>
+    </header>
+  );
+};
+
+export default Header;
